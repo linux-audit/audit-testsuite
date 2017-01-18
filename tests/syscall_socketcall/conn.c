d0835aeaaac9987e2c5cf667ae9930f48b20f715
Paul Moore <paul@paul-moore.com>
2017-01-18 16:01:50 -0500

    all: fix the C syntax/style
    Fixes provided by ./tools/check-syntax.
    
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- tests/syscall_socketcall/conn.c
+++ tests/syscall_socketcall/conn.c
@@ -7,71 +7,73 @@
 
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-   int sockfd, portno, n;
-   struct sockaddr_in serv_addr;
-   struct hostent *server;
+int main(int argc, char *argv[])
+{
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-   char buffer[256];
+	char buffer[256];
 
-   if (argc < 3) {
-      fprintf(stderr,"usage %s hostname port\n", argv[0]);
-      exit(0);
-   }
+	if (argc < 3) {
+		fprintf(stderr, "usage %s hostname port\n", argv[0]);
+		exit(0);
+	}
 
-   portno = atoi(argv[2]);
+	portno = atoi(argv[2]);
 
-   /* Create a socket point */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	/* Create a socket point */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
-   }
+	if (sockfd < 0) {
+		perror("ERROR opening socket");
+		exit(1);
+	}
 
-   server = gethostbyname(argv[1]);
+	server = gethostbyname(argv[1]);
 
-   if (server == NULL) {
-      fprintf(stderr,"ERROR, no such host\n");
-      exit(0);
-   }
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
 
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
+	      server->h_length);
+	serv_addr.sin_port = htons(portno);
 
-   /* Now connect to the server */
-   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR connecting");
-      exit(1);
-   }
+	/* Now connect to the server */
+	if (connect(sockfd,
+		    (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("ERROR connecting");
+		exit(1);
+	}
 
-   /* Now ask for a message from the user, this message
-      * will be read by server
-   */
+	/* Now ask for a message from the user, this message
+	 * will be read by server */
 
-   printf("Please enter the message: ");
-   bzero(buffer,256);
-   fgets(buffer,255,stdin);
+	printf("Please enter the message: ");
+	bzero(buffer, 256);
+	fgets(buffer, 255, stdin);
 
-   /* Send message to the server */
-   n = write(sockfd, buffer, strlen(buffer));
+	/* Send message to the server */
+	n = write(sockfd, buffer, strlen(buffer));
 
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
+	if (n < 0) {
+		perror("ERROR writing to socket");
+		exit(1);
+	}
 
-   /* Now read server response */
-   bzero(buffer,256);
-   n = read(sockfd, buffer, 255);
+	/* Now read server response */
+	bzero(buffer, 256);
+	n = read(sockfd, buffer, 255);
 
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
+	if (n < 0) {
+		perror("ERROR reading from socket");
+		exit(1);
+	}
 
-   printf("%s\n",buffer);
-   return 0;
+	printf("%s\n", buffer);
+	return 0;
 }
